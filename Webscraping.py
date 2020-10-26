10c0f0d09bbd2e437d0fb08ab407e1e222910374
Jennifer Ollett <jenniferollett@gmail.com>
2020-10-26 19:11:16 +0000

    Update Webscraping.py

--- Webscraping.py
+++ Webscraping.py
@@ -1,15 +1,18 @@
 import time
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
 import urllib.request
 import os
 
-dir_path = "D:\\Jen\\Documents\\Dissertation\\when\\"
+dir_path = "D:\\Jen\\Documents\\Dissertation\\Bee_Data\\"
 file_name = "bee"
+image_num = 2462
 
 def download_image(url, num):
     print("[INFO] downloading {}".format(url))
@@ -17,9 +20,26 @@ def download_image(url, num):
     urllib.request.urlretrieve(url, dir_path + file_name + str(num) + ".jpg")
 
 def get_bee_data(num):
-        for image in driver.find_elements(By.CLASS_NAME, "fancybox"):
-            download_image(image.get_attribute("href"), num)
-            num += 1
+        #for image in driver.find_elements(By.CLASS_NAME, "fancybox"):
+            #download_image(image.get_attribute("href"), num)
+            #num += 1
+        table = driver.find_element(By.ID, "report-grid-0")
+        csv_path = dir_path + "Bee_Data.csv"
+        with open(csv_path, 'a', newline='') as f:
+            beewriter = csv.writer(f)
+            for row in table.find_elements(By.TAG_NAME, "tr"):
+                row_list = []
+                if ("empty-row" != row.get_attribute("class")) and ("row18084047" != row.get_attribute("class")):
+                    for cell in row.find_elements(By.TAG_NAME, "td"):
+                        if "col-images" in cell.get_attribute("class"):
+                            image = cell.find_element(By.CLASS_NAME, "fancybox")
+                            download_image(image.get_attribute("href"), num)
+                            image_name = "bee" + str(num)
+                            row_list.append(image_name)
+                            num += 1
+                        elif ("col-actions" not in cell.get_attribute("class")) and ("footable-visible" != cell.get_attribute("class")):
+                            row_list.append(cell.text)
+                beewriter.writerow(row_list)
         return num
 
 with webdriver.Firefox() as driver:
@@ -49,15 +69,22 @@ with webdriver.Firefox() as driver:
     driver.find_element_by_xpath("//div[@id='controls-filter_what']/form[@class='filter-controls']/fieldset/button[@class='fb-apply']").click()
     #driver.find_element(By.ID,"controls-filter_what").find_element(By.CLASS_NAME, "fb-apply").click()
     time.sleep(1)
+    driver.find_element_by_xpath('//a[contains(@href,"#controls-filter_quality")]').click()
+    time.sleep(1)
+    Select(driver.find_element(By.NAME, "has_photos")).select_by_visible_text('Only include records which have photos')
+    time.sleep(1)
+    driver.find_element_by_xpath("//div[@id='controls-filter_quality']/form[@class='filter-controls']/fieldset/button[@class='fb-apply']").click()
+    time.sleep(1)
+
     driver.find_element_by_xpath('//a[contains(@href,"#controls-filter_when")]').click()
     time.sleep(1)
-    driver.find_element(By.ID, "date_from").send_keys("15/10/2020" + Keys.ENTER)
+    driver.find_element(By.ID, "date_from").send_keys("01/06/2020" + Keys.ENTER)
     time.sleep(1)
-    driver.find_element(By.ID, "date_to").send_keys("16/10/2020" + Keys.ENTER)
+    driver.find_element(By.ID, "date_to").send_keys("30/06/2020" + Keys.ENTER)
     time.sleep(1)
     driver.find_element_by_xpath("//div[@id='controls-filter_when']/form[@class='filter-controls']/fieldset/button[@class='fb-apply']").click()
-    time.sleep(15)
-    image_num = 1
+
+    input("Press Enter to continue...")
     image_num = get_bee_data(image_num)
     page_num = 2
     while driver.find_elements(By.ID, "page-report-grid-0-" + str(page_num)): #checks if there are any pages left
