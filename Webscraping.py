65a573f3d897801776e9e0cc202d0832688c66b1
Jennifer Ollett <jenniferollett@gmail.com>
2020-11-20 18:04:10 +0000

    Changed for website update

--- Webscraping.py
+++ Webscraping.py
@@ -10,10 +10,9 @@ import os
 import urllib.request
 from urllib.error import HTTPError
 
-dir_path = "D:\\Jen\\Documents\\Dissertation\\Bee_Data\\"
+dir_path = "D:\\Jen\\Documents\\Dissertation\\Bee_Data_2\\"
 file_name = "bee"
-image_num = 9474
-day = "22/7/2019"
+image_num = 1
 
 def download_image(url, num):
     print("[INFO] downloading {}".format(url))
@@ -30,15 +29,15 @@ def download_image(url, num):
 
 def get_bee_data(num):
         no_image = False
-        table = driver.find_element(By.ID, "report-grid-0")
+        table = driver.find_element(By.ID, "explore-records-grid")
         csv_path = dir_path + "Bee_Data.csv"
         with open(csv_path, 'a', newline='') as f:
             beewriter = csv.writer(f)
-            for row in table.find_elements(By.TAG_NAME, "tr"):
+            for row in driver.find_elements(By.CLASS_NAME, "data-row"):
                 row_list = []
                 if ("empty-row" != row.get_attribute("class")) and ("row18084047" != row.get_attribute("class")):
                     for cell in row.find_elements(By.TAG_NAME, "td"):
-                        if "col-images" in cell.get_attribute("class"):
+                        if "col-12" in cell.get_attribute("class"):
                             image = cell.find_element(By.CLASS_NAME, "fancybox")
                             if row_list[0] not in ["18084047", "12597517", "12504393", "12501491"]:
                                 no_image = download_image(image.get_attribute("href"), num)
@@ -48,11 +47,11 @@ def get_bee_data(num):
                             row_list.append(image_name)
                             if not no_image:
                                 num += 1
-                        elif ("col-actions" not in cell.get_attribute("class")) and ("footable-visible" != cell.get_attribute("class")):
+                        elif not(any(i in cell.get_attribute("class") for i in ["col-2", "col-5", "col-11", "col-actions"])) and ("footable-visible" != cell.get_attribute("class")):
                             row_list.append(cell.text)
                 if (not no_image) and (row_list):
                     try:
-                        beewriter.writerow(row_list)
+                        beewriter.writerow(row_list[2:])
                     except:
                         num -=1
         return num
@@ -62,12 +61,9 @@ with webdriver.Firefox() as driver:
     driver.get("https://www.brc.ac.uk/irecord")
     driver.find_element(By.ID, "edit-name").send_keys("jmollett1@sheffield.ac.uk" + Keys.TAB)
     driver.find_element(By.ID, "edit-pass").send_keys(password + Keys.ENTER)
-    time.sleep(15)
-    driver.find_element(By.ID, "menu-3718-1").click()#send_keys("m.t.smith@sheffield.ac.uk" + Keys.TAB)    
-    time.sleep(1)
-    #first_result = wait.until(presence_of_element_located((By.ID, "menu-3718-1")))
-    driver.find_element(By.ID, "menu-1112-1").click()
-    time.sleep(1)
+    time.sleep(5)
+    driver.get("https://www.brc.ac.uk/irecord/all-records")
+    time.sleep(5)
     driver.find_element(By.ID, "filter-build").click()
     time.sleep(1)
     driver.find_element(By.CLASS_NAME, "fb-filter-link").click()
@@ -75,7 +71,7 @@ with webdriver.Firefox() as driver:
     driver.find_element(By.ID, "ui-id-2").click()
     time.sleep(1)
     driver.find_element(By.ID, "taxa_taxon_list_list:search:searchterm").send_keys("Bombus")
-    time.sleep(1)
+    time.sleep(3)
     driver.find_element(By.ID, "taxa_taxon_list_list:search:searchterm").send_keys(Keys.ENTER)
     time.sleep(1)
     driver.find_element(By.ID, "taxa_taxon_list_list:add").click()
@@ -91,20 +87,9 @@ with webdriver.Firefox() as driver:
     driver.find_element_by_xpath("//div[@id='controls-filter_quality']/form[@class='filter-controls']/fieldset/button[@class='fb-apply']").click()
     time.sleep(1)
 
-    driver.find_element_by_xpath('//a[contains(@href,"#controls-filter_when")]').click()
-    time.sleep(1)
-    driver.find_element(By.ID, "date_from").send_keys(day + Keys.ENTER)
-    time.sleep(1)
-    driver.find_element(By.ID, "date_to").send_keys(day + Keys.ENTER)
-    time.sleep(1)
-    driver.find_element_by_xpath("//div[@id='controls-filter_when']/form[@class='filter-controls']/fieldset/button[@class='fb-apply']").click()
-
     input("Press Enter to continue...")
     image_num = get_bee_data(image_num)
-    page_num = 2
-    while driver.find_elements(By.ID, "page-report-grid-0-" + str(page_num)): #checks if there are any pages left
-        driver.find_element(By.ID, "page-report-grid-0-" + str(page_num)).click()
-        time.sleep(15)
-        input("Press Enter to continue...")
+    while driver.find_element(By.CLASS_NAME, "next").is_enabled(): #checks if there are any pages left
+        driver.find_element(By.CLASS_NAME, "next").click()
+        time.sleep(30)
         image_num = get_bee_data(image_num)
-        page_num += 1
